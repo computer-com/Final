0c47194280aa63aa83220db6909988fff7a08f84
HNP2135 <hpatel5575@conestogac.on.ca>
2024-11-14 16:19:18 -0500

    renamed file named abcd

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+
+namespace BillingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Billing App");
+            printBill1();
+            printBill2();
+            printBill3();
+        }
+
+        static void printBill1()
+        {
+            Console.WriteLine("PrintBill1");
+            string productName = "Pen";
+            int quantityPurchased = 10;
+            double price = 1.1;
+            double totalBill = quantityPurchased * price;
+            Console.WriteLine("Total Bill: " + totalBill);
+        }
+
+        static void printBill2()
+        {
+            Console.WriteLine("PrintBill2");
+
+            Console.WriteLine("Enter product name:");
+            string productName = Console.ReadLine();
+
+            Console.WriteLine("Enter quantity purchased:");
+            int quantityPurchased = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter price per unit:");
+            double price = double.Parse(Console.ReadLine());
+
+            double totalBill = quantityPurchased * price;
+            Console.WriteLine("Total Bill: " + totalBill);
+        }
+
+        static void printBill3()
+        {
+            Console.WriteLine("PrintBill3");
+
+            Console.WriteLine("Enter the number of products:");
+            int numberOfProducts = int.Parse(Console.ReadLine());
+
+            ArrayList totalBills = new ArrayList();
+
+            for (int i = 0; i < numberOfProducts; i++)
+            {
+                Console.WriteLine($"Enter details for product {i + 1}:");
+
+                Console.WriteLine("Enter product name:");
+                string productName = Console.ReadLine();
+
+                Console.WriteLine("Enter quantity purchased:");
+                int quantityPurchased = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter price per unit:");
+                double price = double.Parse(Console.ReadLine());
+
+                double totalBill = quantityPurchased * price;
+                totalBills.Add(totalBill);
+
+                Console.WriteLine($"Total Bill for {productName}: " + totalBill);
+            }
+
+            Console.WriteLine("Total Bills for all products:");
+            foreach (double bill in totalBills)
+            {
+                int billno = 1;
+                Console.WriteLine("Bill No "+billno+1+" :" + bill);
+            }
+        }
+    }
+}
+
+
